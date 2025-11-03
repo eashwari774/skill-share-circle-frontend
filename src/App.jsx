5d34cc85d5bf7dd7f650b66fbf32685d416e5e95
Eashwari <eashwariphalke56@gmail.com>
2025-11-03 22:55:50 +0530

    fix AddSkill import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import AddSkill from "./pages/AddSkill";
+
 
 function App() {
   return (
