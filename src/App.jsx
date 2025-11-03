ef1fd92c9bb6b7cac8603f376a507341af4daf5a
Eashwari <eashwariphalke56@gmail.com>
2025-11-03 17:13:07 +0530

    update api url

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        
+
       </Routes>
     </BrowserRouter>
   );
